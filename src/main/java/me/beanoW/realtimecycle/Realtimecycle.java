465caa3623007e231a986c630bb4e6b58b33461c
beano999 <40875910+beano999@users.noreply.github.com>
2025-03-28 10:10:10 -0400

    Updated Some logic to make it as efficient as possible.

--- src/main/java/me/beanoW/realtimecycle/Realtimecycle.java
+++ src/main/java/me/beanoW/realtimecycle/Realtimecycle.java
@@ -4,18 +4,14 @@ package me.beanoW.realtimecycle;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.ServerTickManager;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.profiling.jfr.event.ServerTickTimeEvent;
 
 import java.time.LocalTime;
 
 public class Realtimecycle implements ModInitializer {
-    private int tickCounter;
-    private double tickPerMin = 16.6666667;
-    private double tickPerSec = 0.27777778;
+    private final double tickPerMin = 16.6666667;
+    private final double tickPerSec = 0.27777778;
 
     @Override
     public void onInitialize() {
@@ -23,17 +19,15 @@ public class Realtimecycle implements ModInitializer {
     }
 
     private void updateTime(MinecraftServer server) {
-        //tickCounter++;
-        //tick counter may help to smooth the sky in game not sure yet
-        //if (tickCounter == 1) {
-            //tickCounter = 0;
-            ServerWorld world = server.getOverworld(); // Get the main world
-            int realWorldHour = LocalTime.now().getHour();  // Get current real-world hour
-            int realWorldMinute = LocalTime.now().getMinute();
-            int realWorldSecond = LocalTime.now().getSecond();
+            var localTime = LocalTime.now();
+
+        ServerWorld world = server.getOverworld(); // Get the main world
+            int realWorldHour = localTime.getHour();  // Get current real-world hour
+            int realWorldMinute = localTime.getMinute();
+            int realWorldSecond = localTime.getSecond();
 
             // Convert IRL time to Minecraft time (0-24000 ticks per day)
-            int minecraftTime = (int) ((realWorldHour * 1000) + (realWorldMinute * (tickPerMin) + (realWorldSecond * ((tickPerMin) / 60))) - 6000); // formula to convert irl time to mc time
+            int minecraftTime = (int) ((realWorldHour * 1000) + (realWorldMinute * (tickPerMin) + (realWorldSecond * (tickPerSec)) - 6000)); // formula to convert irl time to mc time
 
             world.setTimeOfDay(minecraftTime);
         }
