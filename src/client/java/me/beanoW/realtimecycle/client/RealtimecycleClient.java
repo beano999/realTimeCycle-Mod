ad0bf3e1ff4121bf92e2e1487f3cb9bbdac48833
beano999 <40875910+beano999@users.noreply.github.com>
2025-03-27 23:44:42 -0400

    first commit

---
+++ src/client/java/me/beanoW/realtimecycle/client/RealtimecycleClient.java
@@ -0,0 +1,10 @@
+package me.beanoW.realtimecycle.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RealtimecycleClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/beanoW/realtimecycle/Realtimecycle.java
@@ -0,0 +1,10 @@
+package me.beanoW.realtimecycle;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Realtimecycle implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
